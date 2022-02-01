a258328f714187f4daf9004fdf7b470277f519f3
oscar.depierola <odepierola@baufest.com>
2022-02-01 17:06:06 -0500

    Implementacion de campo delay y puerto completado

--- Molinos.Orquest.Dominio/Recursos/Textos.Designer.cs
+++ Molinos.Orquest.Dominio/Recursos/Textos.Designer.cs
@@ -19,7 +19,7 @@ namespace Molinos.Orquest.Dominio.Recursos {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Textos {
@@ -2154,6 +2154,24 @@ namespace Molinos.Orquest.Dominio.Recursos {
         public static string Intercomunicador_Modificar {
             get {
                 return ResourceManager.GetString("Intercomunicador_Modificar", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Puerto de Audio.
+        /// </summary>
+        public static string Intercomunicador_PuertoAudio {
+            get {
+                return ResourceManager.GetString("Intercomunicador_PuertoAudio", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tiempo Máximo Ejecución.
+        /// </summary>
+        public static string Intercomunicador_TiempoMaximoEjecucion {
+            get {
+                return ResourceManager.GetString("Intercomunicador_TiempoMaximoEjecucion", resourceCulture);
             }
         }
         
