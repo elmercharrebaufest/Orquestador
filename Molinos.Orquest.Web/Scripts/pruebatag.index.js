451752bb78bb07ba0be17a9b9de93ea6186e3400
Luciano Ciotola <lciotola@baufest.com>
2022-02-21 16:27:15 -0300

    Merged PR 167: Fix-Historian-Tag
    Se corrige detalle en el js, para que la pag lo detecte correctamente.

--- Molinos.Orquest.Web/Scripts/pruebatag.index.js
+++ Molinos.Orquest.Web/Scripts/pruebatag.index.js
@@ -1,14 +1,16 @@
-﻿$("#boton-tag").click(function () {
-    var self = $("#boton-tag");
-    var div = $("#resultado-peso");
-    div.html(div.data().textoCarga);
+﻿$(function() {
+    $("#boton-tag").click(function () {
+        var self = $("#boton-tag");
+        var div = $("#resultado-peso");
+        div.html(div.data().textoCarga);
 
-    $.getJSON(self.data().urlActivacion, function (resultado) {
-          
-        if (resultado.Codigo == 0) {
-            div.html(resultado.Mensaje);
-        } else {
-            div.html(resultado.Mensaje);
-        }
+        $.getJSON(self.data().urlActivacion, function (resultado) {
+
+            if (resultado.Codigo == 0) {
+                div.html(resultado.Mensaje);
+            } else {
+                div.html(resultado.Mensaje);
+            }
+        });
     });
 });
