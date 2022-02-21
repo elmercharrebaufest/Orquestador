9193669e296da8bf2d51e3da171ef39a7156c94a
Luciano Ciotola <lciotola@baufest.com>
2022-02-21 12:12:41 -0300

    Merged PR 164: SCT310-TagHistorian
    SCT310-TagHistorian
    
    Arreglos visuales + se agrega llamada a pruebas historian  desde tags

---
+++ Molinos.Orquest.Web/Scripts/pruebatag.index.js
@@ -0,0 +1,14 @@
+﻿$("#boton-tag").click(function () {
+    var self = $("#boton-tag");
+    var div = $("#resultado-peso");
+    div.html(div.data().textoCarga);
+
+    $.getJSON(self.data().urlActivacion, function (resultado) {
+          
+        if (resultado.Codigo == 0) {
+            div.html(resultado.Mensaje);
+        } else {
+            div.html(resultado.Mensaje);
+        }
+    });
+});
