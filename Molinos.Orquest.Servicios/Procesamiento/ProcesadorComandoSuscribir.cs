f715eb9fa87d3c28e8fe36379e9a188cc7d37d8c
Oscar De Pierola <odepierola@baufest.com>
2023-06-02 13:20:51 -0500

    fix log

--- Molinos.Orquest.Servicios/Procesamiento/ProcesadorComandoSuscribir.cs
+++ Molinos.Orquest.Servicios/Procesamiento/ProcesadorComandoSuscribir.cs
@@ -19,12 +19,12 @@ namespace Molinos.Orquest.Servicios.Procesamiento
 
         protected override ResultadoSuscribir Ejecutar(ComandoSuscribir comando, Dispositivo dispositivo, IDriver driver)
         {
-            log.Info("--OSCAR LecturaTarjetaRecibida 3. " + comando.CodigoEvento);
+            Log.Info("--OSCAR LecturaTarjetaRecibida 3. " + comando.CodigoEvento);
             ResultadoSuscribir resultado;
             if (driver.EventosSoportados.Any(evento => evento == comando.CodigoEvento))
             {
                 var idSuscripcion = adminSuscripciones.CrearSuscripcion(dispositivo.Id, comando.CodigoEvento, comando.RutaAccesoSuscriptor, comando.Persistente);
-                log.Info("--OSCAR LecturaTarjetaRecibida 4.");
+                Log.Info("--OSCAR LecturaTarjetaRecibida 4.");
                 resultado = new ResultadoSuscribir
                     {
                         Mensaje = Mensaje.ResultadoOK(),
@@ -33,13 +33,13 @@ namespace Molinos.Orquest.Servicios.Procesamiento
                 if (comando.CodigoEvento == CodigosEventos.ConexionDispositivoCorrecta ||
                     comando.CodigoEvento == CodigosEventos.ErrorConexionDispositivo)
                 {
-                    log.Info("--OSCAR LecturaTarjetaRecibida 5.");
+                    Log.Info("--OSCAR LecturaTarjetaRecibida 5.");
                     driver.InformarEstado();
                 }
             }
             else
             {
-                log.Info("--OSCAR LecturaTarjetaRecibida 6.");
+                Log.Info("--OSCAR LecturaTarjetaRecibida 6.");
                 resultado = new ResultadoSuscribir
                     {
                         Mensaje = new Mensaje(Codigos.EventoNoSoportado, Textos.ResultadoEventoNoSoportadoPorDriver,
