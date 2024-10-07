0f7d0441274e9db12ed1eb1a4edc53f79849d3d6
GuilleSanz <ggsanchez@baufest.com>
2024-10-07 14:17:45 -0500

    add script ps1

---
+++ InstallationGuide/Install-RolesAndFeatures.ps1
@@ -0,0 +1,32 @@
+function Install-FeaturesFromCsv {
+    param (
+        [string]$importPath
+    )
+
+    # Verificar si el archivo CSV existe
+    if (-Not (Test-Path $importPath)) {
+        Write-Host "El archivo CSV $importPath no se encuentra."
+        return
+    }
+
+    # Leer el archivo CSV e instalar las features
+    $featuresToInstall = Import-Csv -Path $importPath | Select-Object -ExpandProperty Name
+
+    foreach ($feature in $featuresToInstall) {
+        # Verificar si la feature ya está instalada
+        $installedFeature = Get-WindowsFeature -Name $feature
+        if ($installedFeature.Installed -eq $false) {
+            try {
+                Install-WindowsFeature -Name $feature -ErrorAction Stop
+                Write-Host "Feature $feature instalada correctamente."
+            }
+            catch {
+                Write-Host "Error al instalar la feature $feature: $_"
+            }
+        }
+        else {
+            Write-Host "Feature $feature ya está instalada."
+        }
+    }
+}
+Install-FeaturesFromCsv -importPath "C:\InstalledFeatures.csv"
